2b546c341889762a80ce7a46fd54e9b97014c91b
rahilsk203 <146503651+rahilsk203@users.noreply.github.com>
2024-05-21 23:02:59 +0530

    Update main_server.py

--- main_server.py
+++ main_server.py
@@ -12,6 +12,7 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import cosine_similarity
+from cachetools import TTLCache
 
 app = Flask(__name__)
 CORS(app)
@@ -22,27 +23,27 @@ server_key = "wallartify2024new"
 
 # Setup CSV file for logging queries
 def setup_csv():
-    try:
-        if not os.path.exists('ip_query_log.csv'):
-            with open('ip_query_log.csv', 'w', newline='') as file:
+    csv_file = 'ip_query_log.csv'
+    if not os.path.exists(csv_file):
+        try:
+            with open(csv_file, 'w', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(["ID", "IP Address", "Query", "Timestamp", "Response Status"])
-    except Exception as e:
-        print(f"Error setting up CSV file: {e}")
+        except Exception as e:
+            print(f"Error setting up CSV file: {e}")
 
 # Dictionary to store client IPs and their corresponding IDs
 client_ids = {}
 
 # Log query to CSV
-def log_query(ip_address, query, response_success):
+def log_query(ip_address, query, response_success, file):
     try:
         if ip_address not in client_ids:
             client_ids[ip_address] = str(uuid.uuid4())
         unique_id = client_ids[ip_address]
-        with open('ip_query_log.csv', 'a', newline='') as file:
-            writer = csv.writer(file)
-            timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
-            writer.writerow([unique_id, ip_address, query.lower(), timestamp, response_success])
+        writer = csv.writer(file)
+        timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+        writer.writerow([unique_id, ip_address, query.lower(), timestamp, response_success])
     except Exception as e:
         print(f"Error logging query: {e}")
 
@@ -55,12 +56,19 @@ def validate_key(request):
         print(f"Error validating client key: {e}")
         return False
 
+# Caching results of the search for 5 minutes
+cache = TTLCache(maxsize=100, ttl=300)
+
 # Search wallpapers using PeakPx API
 def search_wallpapers(query):
+    if query in cache:
+        return cache[query], True
+
     try:
         wallpapers = px.search_wallpapers(query=query)
         if wallpapers:
             image_urls = [wallpaper['url'] for wallpaper in wallpapers]
+            cache[query] = image_urls
             return image_urls, True
         else:
             return [], False
@@ -86,7 +94,6 @@ def partial_query_recommendation(partial_query, queries, vectorizer, model, top_
         unique_recommendations = []
         seen = set()
         for idx in sorted_indices:
-            # Check if the query starts with the input prefix
             if queries[idx].startswith(partial_query.lower()) and queries[idx] not in seen:
                 unique_recommendations.append(queries[idx])
                 seen.add(queries[idx])
@@ -101,89 +108,83 @@ def partial_query_recommendation(partial_query, queries, vectorizer, model, top_
 # Load data from CSV file for logging queries
 def load_data(filename):
     queries = []
-    with open(filename, 'r', newline='') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            queries.append(row['Query'].lower())
+    try:
+        with open(filename, 'r', newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                queries.append(row['Query'].lower())
+    except Exception as e:
+        print(f"Error loading data: {e}")
     return queries
 
 # API endpoint to search wallpapers
 @app.route('/search_wallpapers', methods=['GET'])
 def search_wallpapers_route():
-    try:
-        if not validate_key(request):
-            print("Invalid client key")
-            return jsonify({'error': 'Invalid client key'}), 401
+    if not validate_key(request):
+        return jsonify({'error': 'Invalid client key'}), 401
 
-        query = request.args.get('query')
-        if query:
-            client_ip = request.remote_addr  # Get IP from request
-            print(f"Request from IP: {client_ip}, Query: {query}")
-            image_urls, success = search_wallpapers(query)
-            log_query(client_ip, query, success)
-            if success:
-                response_data = [{'Image': url} for url in image_urls]
-                return jsonify(response_data), 200
-            else:
-                return jsonify({'error': 'No wallpapers found for the given query'}), 404
-        else:
-            print("Query parameter is required")
-            return jsonify({'error': 'Query parameter is required'}), 400
-    except Exception as e:
-        print(f"Error in search_wallpapers_route: {e}")
-        return jsonify({'error': 'An unexpected error occurred'}), 500
+    query = request.args.get('query')
+    if not query:
+        return jsonify({'error': 'Query parameter is required'}), 400
+
+    client_ip = request.remote_addr
+    image_urls, success = search_wallpapers(query)
+    with open('ip_query_log.csv', 'a', newline='') as file:
+        log_query(client_ip, query, success, file)
+
+    if success:
+        response_data = [{'Image': url} for url in image_urls]
+        return jsonify(response_data), 200
+    else:
+        return jsonify({'error': 'No wallpapers found for the given query'}), 404
 
 # API endpoint to view logs
 @app.route('/view_logs', methods=['GET'])
 def view_logs():
-    try:
-        if not validate_key(request):
-            print("Invalid client key")
-            return jsonify({'error': 'Invalid client key'}), 401
+    if not validate_key(request):
+        return jsonify({'error': 'Invalid client key'}), 401
 
-        logs = []
+    logs = []
+    try:
         with open('ip_query_log.csv', 'r', newline='') as file:
             reader = csv.DictReader(file)
             for row in reader:
                 logs.append({'ID': row['ID'], 'IP Address': row['IP Address'], 'Query': row['Query'], 'Timestamp': row['Timestamp'], 'Response Success': row['Response Status']})
-        return jsonify(logs), 200
     except Exception as e:
-        print(f"Error in view_logs: {e}")
         return jsonify({'error': 'An unexpected error occurred'}), 500
+
+    return jsonify(logs), 200
 
 # API endpoint to get recommendations for partial queries
 @app.route('/recommendations', methods=['GET'])
 def get_recommendations():
-    try:
-        if not validate_key(request):
-            print("Invalid client key")
-            return jsonify({'error': 'Invalid client key'}), 401
+    if not validate_key(request):
+        return jsonify({'error': 'Invalid client key'}), 401
 
-        partial_query = request.args.get('q')
-        if partial_query:
-            queries = load_data('ip_query_log.csv')
-            vectorizer, model = train_model(queries)
-            recommendations = partial_query_recommendation(partial_query, queries, vectorizer, model)
-            recommendations_list = [{'recommend': recommendation} for recommendation in recommendations]
-            return jsonify(recommendations_list), 200
-        else:
-            return jsonify({'error': 'Partial query parameter (q) is required'}), 400
-    except Exception as e:
-        print(f"Error in get_recommendations: {e}")
-        return jsonify({'error': 'An unexpected error occurred'}), 500
+    partial_query = request.args.get('q')
+    if not partial_query:
+        return jsonify({'error': 'Partial query parameter (q) is required'}), 400
+
+    queries = load_data('ip_query_log.csv')
+    vectorizer, model = train_model(queries)
+    recommendations = partial_query_recommendation(partial_query, queries, vectorizer, model)
+    recommendations_list = [{'recommend': recommendation} for recommendation in recommendations]
+
+    return jsonify(recommendations_list), 200
 
 def run_flask_app():
+    setup_csv()
+
     try:
-        setup_csv()  # Set up the CSV file when the server starts
         print("Starting server...")
         time.sleep(1)
         print("Server started successfully!")
-        
-        subprocess.Popen(["playit"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)  # Start playit with default settings
+
+        playit_proc = subprocess.Popen(["playit"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         time.sleep(2)
         print("Playit started successfully!")
-        
-        subprocess.run(["gunicorn", "--bind", "0.0.0.0:5000", "--workers", "2", "--preload", "--threads", "2", "main:app"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)  # Hide Gunicorn logs
+
+        subprocess.run(["gunicorn", "--bind", "0.0.0.0:5000", "--workers", "4", "--threads", "2", "main:app"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     except Exception as e:
         print(f"Error running Flask app: {e}")
 
@@ -192,5 +193,5 @@ def signal_handler(sig, frame):
     exit(0)
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal_handler)  # Register signal handler for Ctrl+C
+    signal.signal(signal.SIGINT, signal_handler)
     run_flask_app()
